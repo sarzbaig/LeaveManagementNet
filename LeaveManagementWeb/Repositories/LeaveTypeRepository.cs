6af2a9f6559675dcd69085cf1b3c2d54549a8900
sarah <sarah@DESKTOP-1HV9UFV>
2024-02-25 20:08:56 -0600

    Refactored code to have generic-- Leave Type Repository.

---
+++ LeaveManagementWeb/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagementWeb.Data;
+using LeaveManagementWeb.Contracts;
+namespace LeaveManagementWeb.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        public LeaveTypeRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
