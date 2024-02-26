aaaa7f273e6fbee3f07258b238515e94f31a30a2
sarah <sarah@DESKTOP-1HV9UFV>
2024-02-25 18:32:08 -0600

    Modified the crud screens and added datatables and swal.

---
+++ LeaveManagementWeb/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using LeaveManagementWeb.Data;
+using LeaveManagementWeb.Models;
+using AutoMapper; 
+using Microsoft.Build.Framework.Profiler;
+
+namespace LeaveManagementWeb.Configurations
+{
+    public class MapperConfig : Profile  
+    {
+        public MapperConfig() {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
